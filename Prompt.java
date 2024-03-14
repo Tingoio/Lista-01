dcd03765ed747289a957d86fc960c76ee481bc93
Tingoio <tingoiogamer@gmail.com>
2024-03-14 11:07:46 -0300

    Att .gitignore

--- Prompt.java
+++ Prompt.java
@@ -2,6 +2,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
+//Att
 /**
  * Um classe utilitária para imprimir e ler textos, números inteiros e decimais
  * no prompt de comandos.
