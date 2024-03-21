f723f9e02eecbb041777062052f9051fb1fa8dbd
Tingoio <tingoiogamer@gmail.com>
2024-03-21 09:10:22 -0300

    Exercicio15

--- Exercicio15.java
+++ Exercicio15.java
@@ -1,9 +1,9 @@
 public class Exercicio15 {
     public static void executar(){
-        int ponto1x = Prompt.lerInteiro("Digite o x do primeiro ponto ");
-        int ponto1y = Prompt.lerInteiro("Digite o y do primeiro ponto ");
-        int ponto2x = Prompt.lerInteiro("Digite o x do segundo ponto ");
-        int ponto2y = Prompt.lerInteiro("Digite o y do segundo ponto ");
+        int ponto1x = Prompt.lerInteiro("Digite o x do primeiro ponto: ");
+        int ponto1y = Prompt.lerInteiro("Digite o y do primeiro ponto: ");
+        int ponto2x = Prompt.lerInteiro("Digite o x do segundo ponto: ");
+        int ponto2y = Prompt.lerInteiro("Digite o y do segundo ponto: ");
 
         double distancia = Math.sqrt(Math.pow((ponto2x - ponto1x),2) + Math.pow ((ponto2y - ponto1y), 2));
         Prompt.imprimir("Distancia: " + distancia);
